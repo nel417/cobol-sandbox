698f8171774c3fa0a563a882226daafe4b6abcd3
nick L <xnel417x@gmail.com>
2020-09-10 19:29:27 -0500

    Figurative constants

--- cbtut.cob
+++ cbtut.cob
@@ -7,25 +7,51 @@ ENVIRONMENT DIVISION.
 
 DATA DIVISION.
 FILE SECTION.
+
 WORKING-STORAGE SECTION.
-01 UserName PIC X(30) VALUE "Nick".
-01 Num1 PIC 9 VALUE ZEROS.
-01 Num2 PIC 9 VALUE ZEROS.
-01 Total PIC 9 Value 0.
-01 SSNum.
-       02 SSArea PIC 999.
-       02 SSGroup PIC 99.
-       02 SSSerial Pic 9999.
-01 PIValue CONSTANT as 3.14.
+01 SampleData PIC X(10) VALUE "Stuff".
+01 JustLetters PIC AAA VALUE "ABC".
+01 JustNums PIC 9(4) VALUE 1234.
+01 SignedInt PIC S9(4) VALUE -1234.
+01 PayCheck PIC 9(4)V99 VALUE ZEROS.
+01 Customer.
+       02 Ident PIC 9(3).
+       02 CustName Pic X(20).
+       02 DateOfBirth.
+           03 MOB PIC 99.
+           03 DOB PIC 99.
+           03 YOB PIC 9(4).
+01 Num1 PIC 9 VALUE 5.
+01 Num2 PIC 9 VALUE 4.
+01 Num3 PIC 9 VALUE 3.
+01 Ans PIC S99V99 VALUE 0.
+01 REM PIC 9v99.
 
 PROCEDURE DIVISION.
-DISPLAY "what is your name " WITH NO ADVANCING
-ACCEPT UserName
-DISPLAY "Hello " UserName
-DISPLAY "Enter 2 numbers "
-ACCEPT Num1
-ACCEPT Num2
-COMPUTE Total = Num1 + Num2
-DISPLAY Num1 " + " Num2 " = " Total
+MOVE "More Stuff" TO SampleData
+MOVE "123" TO SampleData
+MOVE 123 TO SampleData
+DISPLAY SampleData
+DISPLAY PayCheck
+MOVE "123Bob Smith           09241976" TO Customer
+DISPLAY CustName
+DISPLAY MOB "/" DOB "/" YOB
+MOVE ZERO TO SampleData
+DISPLAY SampleData
+MOVE SPACE TO SampleData
+DISPLAY SampleData
+MOVE HIGH-VALUE TO SampleData
+DISPLAY SampleData
+MOVE LOW-VALUE TO SampleData
+DISPLAY SampleData
+MOVE QUOTE TO SampleData
+DISPLAY SampleData
+MOVE ALL "2" TO SampleData
+DISPLAY SampleData
+
+
+
+
+
 STOP RUN.
 
